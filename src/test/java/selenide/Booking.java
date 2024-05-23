2cae2e6117fe4e1aeeec3e1068fb84438be554ad
Eugene <enne.015@yandex.ru>
2024-05-23 14:16:19 +0300

    init

--- src/test/java/selenide/Booking.java
+++ src/test/java/selenide/Booking.java
@@ -2,8 +2,6 @@ package selenide;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Selenide.$;
 
